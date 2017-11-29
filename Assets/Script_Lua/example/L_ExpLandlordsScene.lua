111e94de806c58da8379a3e02b6870a48fa4b952
cheney <247689848@qq.com>
2017-11-29 20:31:08 +0800

    1
    1

--- Assets/Script_Lua/example/L_ExpLandlordsScene.lua
+++ Assets/Script_Lua/example/L_ExpLandlordsScene.lua
@@ -1,5 +1,6 @@
 local GameObject = UnityEngine.GameObject
 local Input = UnityEngine.Input
+local Text = UnityEngine.UI.Text
 local KeyCode = UnityEngine.KeyCode
 
 require "landlords/L_Landlords"
@@ -8,54 +9,95 @@ L_ExpLandlordsScene = {}
 setmetatable(L_ExpLandlordsScene, {__index = _G})
 local _this = L_ExpLandlordsScene
 
+_this.cardIndex = 1
+_this.shunZiList = nil
+
 function _this:Init()
 
     local cardList = {
 
-        {0 , 1 , 0},
-        {1 , 2 , 0},
-        {2 , 5 , 0},
-        {3 , 7 , 0},
+        {0 , 12 , 0},
+        {1 , 13 , 0},
+        {2 , 10 , 0},
+        {3 , 6 , 0},
         {4 , 8 , 0},
-        {5 , 9 , 0},
+        {5 , 2 , 0},
         {6 , 2 , 0},
         {7 , 7 , 0},
-        {8 , 6 , 0},
-        {9 , 4 , 0},
+        {8 , 3 , 1},
+        {9 , 4 , 1},
         {10 , 4 , 0},
         {11 , 3 , 0},
-        {11 , 9 , 0}
+        {12 , 9 , 0}
     }
 
     local list = {}
+    local strText = ""
     for i,v in ipairs(cardList) do
         
         table.insert( list, L_Landlords:CreatCard(v[1] , v[2], v[3]))
+
+        strText = strText .. v[2] .. "  "
     end
+
+    local uCardText = GameObject.Find("TextCard"):GetComponent("Text")
+    uCardText.text = strText
+
     print(string.format( "初始化牌 %s 张", #list ))
 
-    local shunZiList =  L_Landlords:GetShunZi(list)
+    self.shunZiList =  L_Landlords:GetShunZi(list)
 
-    local debugStr = "得出的顺子:\n"
-    for i,v in ipairs(shunZiList) do
-        
+    local debugStr = "得出的顺子列表 " .. #self.shunZiList .. " 个:\n"
+    for i,v in ipairs(self.shunZiList) do
+
         for j,k in ipairs(v) do
             
-            debugStr = debugStr .. "  " .. k[2]
+            if k.lz > 0 then
+
+                debugStr = debugStr .. "  (" .. k.pnum .."x)"
+            else
+                debugStr = debugStr .. "  " .. k.num
+            end
         end
+        -- debugStr = debugStr .. "  " .. v
         debugStr = debugStr .. "\n"
     end
     print(debugStr)
     UpdateBeat:Add(self.Update , self)
 end
 
+function _this:UpdateUI()
+
+    local c = self.shunZiList[self.cardIndex]
+    local t = ""
+    for i,v in ipairs(c) do
+        if v.lz > 0 then
+    
+            t = t .. "  (" .. v.pnum .."x)"
+        else
+            t = t .. "  " .. v.num
+        end
+    end
+    local uoCardText = GameObject.Find("OutPutTextCard"):GetComponent("Text")
+    uoCardText.text = t
+end
+
 function _this:Update()
 
     if Input.GetKeyDown(KeyCode.Alpha1) then
 
-        
+        self:UpdateUI()
+        self.cardIndex = self.cardIndex + 1
+        if self.cardIndex > #self.shunZiList then
+            self.cardIndex = 1
+        end
     elseif Input.GetKeyDown(KeyCode.Alpha2) then
 
-        
+        self:UpdateUI()
+        self.cardIndex = self.cardIndex - 1
+        if self.cardIndex < 1 then
+            self.cardIndex = 1
+        end
     end
 end
+
--- Assets/Script_Lua/landlords/L_Landlords.lua
+++ Assets/Script_Lua/landlords/L_Landlords.lua
@@ -6,15 +6,149 @@ local _this = L_Landlords
 
 function _this:GetShunZi(list)
     
-    local shunZiList = {}
+    if #list < 5 then
+        return
+    end
+    --clone
+    local cloneList = {}
+    for i,v in ipairs(list) do
+        table.insert( cloneList, v )
+    end
+
+    local totalList = {}
+    local tempList = {}
+    local lz = {}
+    local dex = {}
+    for i,v in ipairs(cloneList) do
+        if v.lz > 0 then
+            v.pnum = v.num
+            v.num = 1
+            table.insert( lz, v )
+            dex[#lz] = 1
+        end
+    end
+
+    local len = math.pow(13,#lz)
+    local up = true
+    print("len =", len)
+    for i = 1, len do
+        up = true
+        if i ~= 1 then
+            for j = 1 , #dex do
+                if up then
+                    dex[j] = dex[j] + 1
+                    if dex[j] > 13 then
+                        dex[j] = 1
+                        up = true
+                    else
+                        up = false
+                        break
+                    end
+                end
+            end
+        end
+        
+        -- local str = ""
+        for l=1,#lz do
+            -- str = str .. "  " .. dex[l]
+            lz[l].num =  dex[l]
+        end
+        -- print(str)
+        local combinList = self:Combin(cloneList)
+        for u,y in ipairs(combinList) do
+            
+            local isAdd = true
+            for u2,y2 in ipairs(tempList) do
+                
+                if y2 == y then
+                    isAdd = false
+                end
+            end
+            if isAdd then
+                table.insert(tempList, y)
+            end
+        end
+    end
+
+    for i,v in ipairs(tempList) do
+        
+        --匹配规则
+        --区间匹配
+        local t = {}
+        -- print(v)
+        for s in string.gfind(v, "%d+") do
+            -- print(tonumber(s))
+            table.insert(t , list[tonumber(s) + 1])
+        end
+        -- print("break")
+        table.insert(totalList , t)
+    end
+
     --sort
-    --遍历list 标记lz 卡牌
-    
+    for i = 1 , #totalList do
+        for j = 1 , #totalList - i do
 
+            if #totalList[j] < #totalList[j+1] then
+                
+                tmp = totalList[j]  
+                totalList[j] = totalList[j+1]
+                totalList[j+1] = tmp
+            end
+        end  
+    end
 
-    return shunZiList
+ 
+    return totalList
     --return {{{1 , 2 , 0},{1 , 2 , 0},{1 , 2 , 0}} , {{1 , 2 , 0},{1 , 2 , 0},{1 , 2 , 0}}}
 end
+
+
+function _this:Combin(list)
+    
+    local shunZiList = {}
+    local lzCount = 0
+    --sort
+    local tmp = nil
+    for i = 1 , #list do
+        for j = 1 , #list - i do
+
+            if list[j].num < list[j+1].num then
+                
+                tmp = list[j]  
+                list[j] = list[j+1]
+                list[j+1] = tmp
+            end
+        end  
+    end
+
+    for i = 1, #list do
+
+        local combin = tostring(list[i].id)
+        local num = list[i].num
+        local count = 0
+        for j = i + 1, #list do
+            
+            -- print(string.format( "num = %d , toNum = %d", num , list[j].num))
+            if list[j].num + 1 == num then
+
+                -- table.insert( combin, list[j])
+                combin = combin .. "," .. list[j].id
+                num = list[j].num
+                count = count + 1
+            elseif list[j].num == num then
+            else
+                break
+            end
+        end
+        -- print("break")
+        if count >= 5 then
+            -- print("output")
+            table.insert( shunZiList, combin)
+        end
+    end
+    return shunZiList
+end
+
 
 function _this:CreatCard(id , num , lz)
     
